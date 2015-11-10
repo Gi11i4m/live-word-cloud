5b504b1d8ef2e27795226ec70b6ddb2ce64d5409
Gilliam <gi11i4m@gmail.com>
2015-11-10 13:14:21 +0100

    Smartphone detection fix

--- scripts/main.js
+++ scripts/main.js
@@ -133,6 +133,6 @@ wordCloud.controller('WordCloudUpdateController', function($scope, wordRepositor
 	}
 	
 	function isSmartphone() {
-		return navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i);
+		return window.innerWidth < 600;
 	}
 });
