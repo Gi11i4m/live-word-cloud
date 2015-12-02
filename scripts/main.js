25425a3620037da22038c375138813d55e659c4e
Gilliam <gi11i4m@gmail.com>
2015-12-02 09:48:50 +0000

    Extracted strings to service

--- scripts/main.js
+++ scripts/main.js
@@ -21,15 +21,27 @@ wordCloud.factory('wordRepository', function($http) {
 	};
 });
 
-wordCloud.constant("strings", {
-	WORDS_UNCHANGED_MESSAGE: "Words are unchanged"
+wordCloud.service("strings", function() {
+	this.WORDS_UNCHANGED_MESSAGE = "No words changed";
+	this.WORDS_CHANGED_MESSAGE = "Refreshing words...";
+	this.WORD_DEFAULT = "";
+	this.SUBMIT_BUTTON_DEFAULT = "Geef feedback";
+	this.SUBMIT_BUTTON_ADDING = "Toevoegen...";
+	this.SUBMIT_BUTTON_SUCCESS = "Bedankt!";
+	this.RESET_BUTTON_DEFAULT = "Reset";
+	this.LEGEND_MESSAGE_SUCCESS = "Bedankt voor uw feedback!";
+	this.LEGEND_MESSAGE_DEFAULT = "Omschrijf de ASWFM in één woord";
+	this.VALIDATION_PATTERN = /^[A-Za-z]{2,20}$/;
+	this.VALIDATION_PATTERN_MESSAGE = "zorg dat je woord enkel letters bevat, en niet langer is dan 20 tekens";
+	this.GENERIC_ERROR_MESSAGE = "Er ging iets mis...";
+	this.CARD_IMAGE_URL = "images/sfm.png";
+	this.FONT = "Impact";
+	
 });
 
 wordCloud.controller('WordCloudController', function($timeout, $interval, $log, wordRepository, strings) {
 
-	var WORDS_CHANGED_MESSAGE =  "Refreshing words...";
-	
-	this.font = "Impact";
+	this.font = strings.FONT;
 	this.angleCount = 7;
 	this.angleFrom = -90;
 	this.angleTo = 90;
@@ -48,35 +60,22 @@ wordCloud.controller('WordCloudController', function($timeout, $interval, $log, 
 	}
 
 	function refreshWords() {
-		$log.info(WORDS_CHANGED_MESSAGE + "\nWords: " + this.words);
+		$log.info(strings.WORDS_CHANGED_MESSAGE + "\nWords: " + this.words);
 		$('#go').click();
 	}
 
 	/* EXECUTION LOOP */
-	var wordPoll = $interval(refreshWordsIfChanged, 3000);
+	wordPoll = $interval(refreshWordsIfChanged, 3000);
 });
 
-wordCloud.controller('WordCloudUpdateController', function($log, wordRepository) {
-
-	var WORD_DEFAULT = "";
-	var SUBMIT_BUTTON_DEFAULT = "Geef feedback";
-	var SUBMIT_BUTTON_ADDING = "Toevoegen...";
-	var SUBMIT_BUTTON_SUCCESS = "Bedankt!";
-	var RESET_BUTTON_DEFAULT = "Reset";
-	var LEGEND_MESSAGE_SUCCESS = "Bedankt voor uw feedback!";
-	var LEGEND_MESSAGE_DEFAULT = "Omschrijf de ASWFM in één woord";
-	var VALIDATION_PATTERN = /^[A-Za-z]{2,20}$/;
-	var VALIDATION_PATTERN_MESSAGE = "zorg dat je woord enkel letters bevat, en niet langer is dan 20 tekens";
-	var GENERIC_ERROR_MESSAGE = "Er ging iets mis...";
-	var CARD_IMAGE_URL = "images/sfm.png";
-
-	this.word = WORD_DEFAULT;
-	this.legendMessage = LEGEND_MESSAGE_DEFAULT;
-	this.validationPattern = VALIDATION_PATTERN;
-	this.patternMessage = VALIDATION_PATTERN_MESSAGE;
-	this.submitButtonValue = SUBMIT_BUTTON_DEFAULT;
-	this.resetButtonValue = RESET_BUTTON_DEFAULT;
-	this.imagePath = CARD_IMAGE_URL;
+wordCloud.controller('WordCloudUpdateController', function($log, wordRepository, strings) {
+	this.word = strings.WORD_DEFAULT;
+	this.legendMessage = strings.LEGEND_MESSAGE_DEFAULT;
+	this.validationPattern = strings.VALIDATION_PATTERN;
+	this.patternMessage = strings.VALIDATION_PATTERN_MESSAGE;
+	this.submitButtonValue = strings.SUBMIT_BUTTON_DEFAULT;
+	this.resetButtonValue = strings.RESET_BUTTON_DEFAULT;
+	this.imagePath = strings.CARD_IMAGE_URL;
 	this.isTextfieldDisabled = false;
 	this.isSubmitDisabled = false;
 	this.isResetVisible = false;
@@ -86,7 +85,7 @@ wordCloud.controller('WordCloudUpdateController', function($log, wordRepository)
 	this.submitWord = function() {
 		if (isWordValid()) {
 			this.isSubmitDisabled = true;
-			this.submitButtonValue = SUBMIT_BUTTON_ADDING;
+			this.submitButtonValue = strings.SUBMIT_BUTTON_ADDING;
 			wordRepository.adWord(this.word.trim(), success, error);
 		} else {
 			error(this.patternMessage);
@@ -105,27 +104,27 @@ wordCloud.controller('WordCloudUpdateController', function($log, wordRepository)
 	function success(response) {
 		$log.info(response);
 		
-		this.submitButtonValue = SUBMIT_BUTTON_SUCCESS;
-		this.legendMessage = LEGEND_MESSAGE_SUCCESS;
+		this.submitButtonValue = strings.SUBMIT_BUTTON_SUCCESS;
+		this.legendMessage = strings.LEGEND_MESSAGE_SUCCESS;
 		this.isTextfieldDisabled = true;
 		this.isSubmitDisabled = true;
 	}
 	
 	function error(message) {
-		var errorMessage = message ? message : GENERIC_ERROR_MESSAGE;
+		var errorMessage = message ? message : strings.GENERIC_ERROR_MESSAGE;
 		$log.error(errorMessage);
 		
 		this.legendMessage = errorMessage;
 		this.isSubmitDisabled = false;
-		this.submitButtonValue = SUBMIT_BUTTON_DEFAULT;
+		this.submitButtonValue = strings.SUBMIT_BUTTON_DEFAULT;
 	}
 	
 	function reset() {
-		this.submitButtonValue = SUBMIT_BUTTON_DEFAULT;
-		this.legendMessage = LEGEND_MESSAGE_DEFAULT;
+		this.submitButtonValue = strings.SUBMIT_BUTTON_DEFAULT;
+		this.legendMessage = strings.LEGEND_MESSAGE_DEFAULT;
 		this.isTextfieldDisabled = false;
 		this.isSubmitDisabled = false;
 		this.isResetVisible = false;
-		this.word = WORD_DEFAULT;
+		this.word = strings.WORD_DEFAULT;
 	}
 });
